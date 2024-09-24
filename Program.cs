24fb75e09f8140c400895c993d48698d02bb4cf3
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-09-24 05:04:15 -0500

    [1.006]

--- Program.cs
+++ Program.cs
@@ -5,4 +5,4 @@ string path = Directory.GetCurrentDirectory() + "//nlog.config";
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
 logger.Info("Program started");
 Console.WriteLine("Hello World!");
-logger.Info("Program ended");
+logger.Info("Program ended");//
